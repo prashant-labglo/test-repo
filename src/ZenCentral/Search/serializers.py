92409b64218c26ee362949193c37152f9e0eb017
Nishant Sharma <nishant@prezentium.com>
2018-02-18 11:21:14 +0530

    Complete pagination of search results achieved.

--- src/ZenCentral/Search/serializers.py
+++ src/ZenCentral/Search/serializers.py
@@ -1,30 +1,33 @@
-from rest_framework import serializers
+from collections import OrderedDict
+from rest_framework import serializers, pagination
 from Search.models import SearchResult, SearchResultRating, SearchQuery, SearchIndex
 from Search.models import IndexTypeChoices
 from ZenCentral import fields
+from rest_framework.response import Response
 
 from django.contrib.auth import get_user_model
 
 class SearchResultRatingSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(
-        read_only=True,
-        default=serializers.CurrentUserDefault())
+    #user = serializers.PrimaryKeyRelatedField(
+    #    read_only=True,
+    #    default=serializers.CurrentUserDefault())
     class Meta:
         model = SearchResultRating
-        fields = ('rated', 'user', 'result')
+        fields = ('rated', 'result', 'user')
 
 class SearchResultSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     myRating = serializers.IntegerField(read_only=False)
+    ratings = SearchResultRatingSerializer(many=True)
     class Meta:
         model = SearchResult
         fields = ('id', 'slide', 'rank', 'query', 'ratings', 'myRating')
         read_only_fields = ('id', 'slide', 'rank', 'query', 'ratings')
 
 class NestedSearchResultRatingSerializer(serializers.ModelSerializer):
-    #user = serializers.PrimaryKeyRelatedField(
-    #    read_only=True,
-    #    default=serializers.CurrentUserDefault())
+    user = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault())
     class Meta:
         model = SearchResultRating
         fields = ('rated', 'user')
@@ -33,11 +36,32 @@ class NestedSearchResultSerializer(serializers.ModelSerializer):
     avgRating = serializers.FloatField(read_only=True)
     myRating = serializers.IntegerField(read_only=False)
     id = serializers.IntegerField(read_only=True)
-    def __init__(self, *argv, **kargv):
-        super().__init__(*argv, **kargv)
     class Meta:
         model = SearchResult
         fields = ('id', 'slide', 'avgRating', 'myRating', 'score')
+
+class PaginatedSearchResultListSerializer(serializers.ListSerializer):
+    def to_representation(self, page):
+        """Convert `username` to lowercase."""
+        paginationObj = self.context["paginationObj"]
+        retval = OrderedDict([
+            ('count', page.paginator.count),
+            ('next', paginationObj.get_next_link()),
+            ('previous', paginationObj.get_previous_link()),
+            ('results', super().to_representation(page))
+        ])
+        return retval
+    def to_internal_value(self, data):
+        return super().to_internal_value(data["results"])
+
+    @property
+    def data(self):
+        ret = super(serializers.ListSerializer, self).data
+        return ret
+
+class PaginatedSearchResultSerializer(NestedSearchResultSerializer):
+    class Meta(NestedSearchResultSerializer.Meta):
+        list_serializer_class=PaginatedSearchResultListSerializer
 
 class SearchQuerySerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(read_only=True)
@@ -45,11 +69,20 @@ class SearchQuerySerializer(serializers.HyperlinkedModelSerializer):
     queryJson = serializers.JSONField()
 
     # Making results read only in the API.
-    results = NestedSearchResultSerializer(many=True, read_only=True)
+    results = serializers.SerializerMethodField('paginated_results')
 
     class Meta:
         model = SearchQuery
         fields = ('id', 'index', 'queryJson', 'created', 'results')
+
+    def paginated_results(self, obj):
+        querySet = SearchResult.objects.filter(query=obj)
+        paginationObj = pagination.PageNumberPagination()
+        paginationObj.page_size_query_param = "page_size"
+        paginationObj.page_size = 10
+        paginationObj.paginate_queryset(querySet, self.context['request'])
+        serializer = PaginatedSearchResultSerializer(paginationObj.page, many=True, context={'request': self.context['request'], 'paginationObj': paginationObj})
+        return serializer.data
 
 class SearchIndexSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(read_only=True)
