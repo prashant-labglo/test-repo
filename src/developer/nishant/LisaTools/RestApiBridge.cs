0a4421182fc4ab11caf486e966acfc0e019dc645
Nishant Sharma <nishant@prezentium.com>
2018-02-22 05:33:24 +0530

    Addressed review comments.

--- src/developer/nishant/LisaTools/RestApiBridge.cs
+++ src/developer/nishant/LisaTools/RestApiBridge.cs
@@ -4,8 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
 using Nancy;
+using Nancy.Hosting.Self;
+using Nancy.Extensions;
+using System.IO;
+using System.Net;
 
 namespace LisaTools
 {
+    public class RestApiBridge : NancyModule
+    {
+        public static PowerPoint.Application app;
+        public static string tempDirectory;
+        public RestApiBridge()
+        {
+            tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()) + "\\";
+            Directory.CreateDirectory(tempDirectory);
+
+            Get["/"] = parameters => "RestApiBridge for ZenTools:";
+            Post["/"] = parameters =>
+            {
+                // Get the URL.
+                var url = this.Request.Body.AsString();
+
+                // Download file in the URL.
+                var filePath = tempDirectory + Path.GetFileName(url);
+                var webClient = new WebClient();
+                webClient.DownloadFile(url, filePath);
+
+                var presentationObj = app.Presentations.Open(filePath);
+
+                foreach (PowerPoint.Slide slide in presentationObj.Slides)
+                {
+                    slide.Copy();
+                    break;
+                }
+
+                presentationObj.Close();
+
+                return "Done";
+            };
+        }
+    }
 }
--- src/developer/nishant/LisaTools/ThisAddIn.cs
+++ src/developer/nishant/LisaTools/ThisAddIn.cs
@@ -8,47 +8,9 @@ using PowerPoint = Microsoft.Office.Interop.PowerPoint;
 using Office = Microsoft.Office.Core;
 using Nancy;
 using Nancy.Hosting.Self;
-using System.Threading;
-using Nancy.Extensions;
-using System.IO;
 
 namespace LisaTools
 {
-    public class RestApiBridge : NancyModule
-    {
-        public static PowerPoint.Application app;
-        public static string tempDirectory;
-        public RestApiBridge()
-        {
-            tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()) + "\\";
-            Directory.CreateDirectory(tempDirectory);
-
-            Get["/"] = parameters => "RestApiBridge for ZenTools:";
-            Post["/"] = parameters =>
-            {
-                // Get the URL.
-                var url = this.Request.Body.AsString();
-
-                // Download file in the URL.
-                var filePath = tempDirectory + Path.GetFileName(url);
-                var webClient = new WebClient();
-                webClient.DownloadFile(url, filePath);
-
-                var presentationObj = app.Presentations.Open(filePath);
-
-                foreach (PowerPoint.Slide slide in presentationObj.Slides)
-                {
-                    slide.Copy();
-                    break;
-                }
-
-                presentationObj.Close();
-
-                return "Done";
-            };
-        }
-    }
-
     public partial class LisaToolsAddin
     {
         NancyHost host;
