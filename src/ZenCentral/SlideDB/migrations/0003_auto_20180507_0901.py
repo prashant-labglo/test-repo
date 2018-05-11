06ec1646910f24b7d1207e27c8ce2e4d2c23e7d7
Prashant Yengantiwar <prashant@labglo.com>
2018-05-11 11:58:35 +0530

    Migration dependacies changed.

--- src/ZenCentral/SlideDB/migrations/0003_auto_20180507_0901.py
+++ src/ZenCentral/SlideDB/migrations/0003_auto_20180507_0901.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('SlideDB', '0001_initial'),
+        ('SlideDB', '0002_slide_content'),
     ]
 
     operations = [
